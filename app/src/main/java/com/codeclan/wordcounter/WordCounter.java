82aa11a8c9dbbce6c33f0f66c9ecf5ca0353cb1e
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-03-14 23:10:22 +0000

    finish word count frequencies method for unsorted output

--- app/src/main/java/com/codeclan/wordcounter/WordCounter.java
+++ app/src/main/java/com/codeclan/wordcounter/WordCounter.java
@@ -1,11 +1,14 @@
 package com.codeclan.wordcounter;
 
+import java.util.HashMap;
+
 /**
  * Created by user on 14/03/2017.
  */
 
 public class WordCounter {
 
+//    public enum wordFreqSort { SORTED, UNSORTED;}
 
     public static long wordCount(String input){
         if (input == " " || input == "") {
@@ -35,7 +38,43 @@ public class WordCounter {
             return count;
         }
     }
-    public static String wordFrequencyList(String input) {
-        return "\"A\" : 2, \"B\" : 1";
+    public static String wordFrequencyList(String input ) {
+
+
+        HashMap<String, Integer> wordfreqhash = new HashMap<String, Integer>();
+
+        if (input == " " || input == "") {
+            return "";
+        }
+        // split based on word boundaries.  Can generate empty strings, so need to check whic are valid
+        try {
+            String[] words = input.split("\\W+");
+            for (String word : words) {
+                if(wordfreqhash.containsKey(word)){
+                    wordfreqhash.put(word, wordfreqhash.get(word) + 1);
+                }
+                else {
+                    wordfreqhash.put(word,1);
+                }
+            }
+        }
+        catch(IndexOutOfBoundsException ex){
+            System.out.println("Exception");
+        }
+
+        // assemble return string
+        String returnString = "";
+        Integer maxValue = 1;
+        int i = 0;
+        for (String word : wordfreqhash.keySet()){
+            i++;
+            returnString += "\"" + word +"\" : " + wordfreqhash.get(word);
+            if(i < wordfreqhash.size()) {
+                returnString = returnString + ", ";
+            }
+            maxValue = Math.max(maxValue, wordfreqhash.get(word));
+        }
+//        return "\"A\" : 2, \"B\" : 1,";
+        return returnString;
     }
 }
